ecdec598e26d5ad05a69a438dfdab1a1e8262e1c
Kuchukov Dmitry <kudim@inbox.ru>
2022-04-07 12:52:15 +0300

    correct React18

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import {createRoot} from "react-dom/client"
+import App from './App'
+import {Provider} from "react-redux"
+import {store} from "./store"
+
+const container = document.getElementById('root')
+
+// @ts-ignore
+const root = createRoot(container)
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+root.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>)
